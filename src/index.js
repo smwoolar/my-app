da6ffc14f7dd094c03fc4054a8f7476d94bb2b1c
Sean Woolard <woolard.34@osu.edu>
2023-01-25 20:04:31 -0500

    updated index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1 class="post-title">Hello World!</h1>
+    <h1 class="greeting">Hello World!</h1>
   </React.StrictMode>
 );
 
